f723e9280acbb2beaf414a4eba330aec6526ce57
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-10-28 08:47:44 -0500

    Herencia

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 
-class animal {
+class Animal {
 	constructor(especie, edad, color) {
 		this.especie = especie; // this es el objeto - // especie parámetro - el objeto "this" tendrá una carácteristica
 		this.edad = edad;
 		this.color = color;
 		this.informacion = `Soy un ${this.especie}, tengo ${this.edad}, 
-		años y soy de color ${this.color}`;
+		años, soy de color ${this.color}, y mi raza es ${this.raza}`;
 	}
 	verInformacion = () => {
 		document.write(this.informacion + "<br>") 
 	};
+} 
+
+class Perro extends Animal {
+	constructor(especie, edad, color, raza) {
+		super(especie, edad, color);
+		this.raza = raza;
+	};
 	ladrar() {
-		if (this.especie == "perro") {
-			document.write("¡Waw! <br>");
-		} else {
-			document.write("No puede ladrar, ya que es un " + this.especie + "<br> ");
-		}
+		alert("¡WAW!");
 	}
 }
 // const siempre para objetos
-const perro = new animal("perro", 5, "marrón");
-const gato = new animal("felino", 2, "negro");
-const pajaro = new animal("loro", 1, "verde");
+const perro = new Perro("perro", 5, "marrón", "rotwailer");
+const gato = new Animal("felino", 2, "negro");
+const pajaro = new Animal("loro", 1, "verde");
 
 perro.ladrar();
-gato.ladrar();
-pajaro.ladrar();
