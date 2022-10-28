a9b910f56374bffa2b09e5b6757d80c0cd33b826
KenpachiPN <sprada71@gmail.com>
2022-10-27 19:41:04 -0500

    Conceptos básicos de la POO

--- main.js
+++ main.js
@@ -1,51 +1,21 @@
 
-const sumar = (num1, num2) => {
-	return parseInt(num1) + parseInt(num2)
-};
-
-const restar = (num1, num2) => {
-	return parseInt(num1) - parseInt(num2)
-};
-
-const multiplicar = (num1, num2) => {
-	return parseInt(num1) * parseInt(num2)
-};
-
-const dividir = (num1, num2) => {
-	return parseInt(num1) / parseInt(num2)
-};
-
-alert("¿Qué operación matemática básica deseas realizar?");
-let operacion = prompt("1: sumar, 2: restar, 3: multiplicar, 4: dividir");
-
-if(operacion == 1) {
-	let numero1 = prompt("Ingrese el primer número");
-	let numero2 = prompt("Ingrese el segundo número");
-	let resul = sumar(numero1,numero2);
-	alert("El resultado de sumar " + numero1 + " y " + numero2 + " es de " + resul);
+class animal {
+	constructor(especie, edad, color) {
+		this.especie = especie; // this es el objeto - // especie parámetro - el objeto "this" tendrá una carácteristica
+		this.edad = edad;
+		this.color = color;
+		this.informacion = `Soy un ${this.especie}, tengo ${this.edad}, 
+		años y soy de color ${this.color}`;
+	}
+	verInformacion = () => {
+		document.write(this.informacion + "<br>") 
+	};
 }
+// const siempre para objetos
+const perro = new animal("canino", 5, "marrón");
+const gato = new animal("felino", 2, "negro");
+const pajaro = new animal("loro", 1, "verde");
 
-else if(operacion == 2) {
-	let numero1 = prompt("Ingrese el primer número");
-	let numero2 = prompt("Ingrese el segundo número");
-	let resul = restar(numero1,numero2);
-	alert("El resultado de restar " + numero1 + " y " + numero2 + " es de " + resul);
-}
-
-else if(operacion == 3) {
-	let numero1 = prompt("Ingrese el primer número");
-	let numero2 = prompt("Ingrese el segundo número");
-	let resul = multiplicar(numero1,numero2);
-	parseFloat(resul);
-	alert("El resultado de multiplicar " + numero1 + " y " + numero2 + " es de " + resul);
-}
-
-else if(operacion == 4) {
-	let numero1 = prompt("Ingrese el primer número");
-	let numero2 = prompt("Ingrese el segundo número");
-	let resul = dividir(numero1,numero2);
-	parseFloat(resul);
-	alert("El resultado de dividir " + numero1 + " y " + numero2 + " es de " + resul);
-} else {
-	alert("No se encontró tu operación");
-}
+perro.verInformacion();
+gato.verInformacion();
+pajaro.verInformacion();
